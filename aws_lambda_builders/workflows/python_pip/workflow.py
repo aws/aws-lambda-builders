76e3c40919fcadb7c7e6e9fba1acc92788067dce
Sanath Kumar Ramesh <sanathkr@amazon.com>
2018-11-12 19:57:48 -0800

    Updating `python_pip` workflow to match BaseWorkflow & BaseAction interfaces

--- aws_lambda_builders/workflows/python_pip/workflow.py
+++ aws_lambda_builders/workflows/python_pip/workflow.py
@@ -1,15 +1,26 @@
-from aws_lambda_builders.workflows import BaseWorkflow
+from aws_lambda_builders.workflow import BaseWorkflow
 from aws_lambda_builders.actions import CopySourceAction
 
 from .actions import PythonPipBuildAction
 
 
 class PythonPipWorkflow(BaseWorkflow):
-    def __init__(self, *args, **kwargs):
-        super(PythonPipWorkflow, self).__init__(*args, **kwargs)
-        # TODO if the parent class just has a self.ACTIONS  that it uses we can
-        # just override that instead of needing to override __init__
-        self.actions= [
-            PythonPipBuildAction,
-            CopySourceAction,
+
+    def __init__(self,
+                 source_dir,
+                 artifacts_dir,
+                 scratch_dir,
+                 manifest_path,
+                 runtime=None, **kwargs):
+
+        super(PythonPipWorkflow, self).__init__(source_dir,
+                                                artifacts_dir,
+                                                scratch_dir,
+                                                manifest_path,
+                                                runtime=runtime,
+                                                **kwargs)
+
+        self.actions = [
+            PythonPipBuildAction(artifacts_dir, manifest_path, runtime),
+            CopySourceAction(source_dir, artifacts_dir),
         ]
