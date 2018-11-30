de3221211069653995c88126ff92fb2c343c9edc
Gojko Adzic <gojko@gojko.com>
2018-11-30 22:22:53 +0100

    tests for changes in osutils

--- tests/functional/workflows/python_pip/test_utils.py
+++ tests/functional/workflows/python_pip/test_utils.py
@@ -2,6 +2,10 @@ import io
 
 import pytest
 
+import sys
+
+import os
+
 from aws_lambda_builders.workflows.python_pip import utils
 
 
@@ -20,3 +24,41 @@ class TestOSUtils(object):
         content = osutils.get_file_contents(filename, binary=False,
                                             encoding='utf-16')
         assert content == checkmark
+
+    def test_find_executable_returns_path_for_known_exe(self, tmpdir, osutils):
+        found = osutils.find_executable('python')
+
+        assert os.path.realpath(found) == os.path.realpath(sys.executable)
+
+    def test_find_executable_returns_none_for_non_existing_exe(self, tmpdir, osutils):
+        found = osutils.find_executable('not-python')
+
+        assert found is None
+
+    def test_dirname_returns_directory_for_path(self, tmpdir, osutils):
+        dirname = osutils.dirname(sys.executable)
+
+        assert dirname == os.path.dirname(sys.executable)
+
+    def test_abspath_returns_absolute_path(self, tmpdir, osutils):
+
+        result = osutils.abspath('.')
+
+        assert os.path.isabs(result)
+
+        assert result == os.path.abspath('.')
+
+    def test_popen_can_accept_cwd(self, tmpdir, osutils):
+
+        testdata_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'testdata')
+
+        p = osutils.popen([sys.executable, 'cwd.py'],
+                          stdout=osutils.pipe,
+                          stderr=osutils.pipe,
+                          cwd=testdata_dir)
+
+        out, err = p.communicate()
+
+        assert p.returncode == 0
+
+        assert out.decode('utf8').strip() == os.path.abspath(testdata_dir)
