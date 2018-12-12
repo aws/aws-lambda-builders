626809264194ca4884b1c2509b1a51cc3fc6fcaa
Alex Wood <awood45@gmail.com>
2018-12-12 13:56:24 -0800

    Integration Test for Dependency Mismatch
    Validates that we get an error from Bundler when dependencies conflict.

--- tests/integration/workflows/ruby_bundler/test_ruby.py
+++ tests/integration/workflows/ruby_bundler/test_ruby.py
@@ -44,3 +44,12 @@ class TestRubyWorkflow(TestCase):
         expected_files = {"handler.rb", "Gemfile", "Gemfile.lock", ".bundle", "vendor"}
         output_files = set(os.listdir(self.artifacts_dir))
         self.assertEquals(expected_files, output_files)
+
+    def test_fails_if_bundler_cannot_resolve_dependencies(self):
+        source_dir = os.path.join(self.TEST_DATA_FOLDER, "broken-deps")
+        with self.assertRaises(WorkflowFailedError) as ctx:
+            self.builder.build(source_dir, self.artifacts_dir, self.scratch_dir,
+                               os.path.join(source_dir, "Gemfile"),
+                               runtime=self.runtime)
+        self.assertIn("RubyBundlerBuilder:RubyBundle - Bundler Failed: ",
+                      str(ctx.exception))
