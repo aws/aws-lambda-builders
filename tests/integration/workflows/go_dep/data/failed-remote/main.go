0f7f433e9094b32b9f04c6909566714cb8645757
Simon Thulbourn <simon+github@thulbourn.com>
2018-12-30 21:58:38 +0100

    adds integration tests

---
+++ tests/integration/workflows/go_dep/data/failed-remote/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "not-really-a-git-repo.com/pkg/log"
+
+func main() {
+	log.Info("hello, world")
+}
---
+++ tests/integration/workflows/go_dep/test_go_dep.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+import tempfile
+
+from unittest import TestCase
+
+from aws_lambda_builders.builder import LambdaBuilder
+from aws_lambda_builders.exceptions import WorkflowFailedError
+
+class TestGoDep(TestCase):
+    TEST_DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")
+
+    def setUp(self):
+        self.artifacts_dir = tempfile.mkdtemp()
+        self.scratch_dir = tempfile.mkdtemp()
+
+        self.no_deps = os.path.join(self.TEST_DATA_FOLDER, "nodeps")
+
+        self.builder = LambdaBuilder(language="go",
+                                     dependency_manager="dep",
+                                     application_framework=None)
+
+        self.runtime = "go1.x"
+
+    def tearDown(self):
+        shutil.rmtree(self.artifacts_dir)
+        shutil.rmtree(self.scratch_dir)
+
+    def test_builds_project_with_no_deps(self):
+        source_dir = os.path.join(self.TEST_DATA_FOLDER, "nodeps")
+
+        self.builder.build(source_dir, self.artifacts_dir, self.scratch_dir,
+                           os.path.join(source_dir, "Gopkg.toml"),
+                           runtime=self.runtime,
+                           options={"handler": "main"})
+
+        expected_files = {"main"}
+        output_files = set(os.listdir(self.artifacts_dir))
+
+        self.assertEquals(expected_files, output_files)
+
+    def test_builds_project_with_no_gopkg_file(self):
+        source_dir = os.path.join(self.TEST_DATA_FOLDER, "no-gopkg")
+
+        with self.assertRaises(WorkflowFailedError) as ex:
+            self.builder.build(source_dir, self.artifacts_dir, self.scratch_dir,
+                            os.path.join(source_dir, "Gopkg.toml"),
+                            runtime=self.runtime,
+                            options={"handler": "main"})
+
+        self.assertEquals(
+            "GoDepBuilder:DepEnsure - Exec Failed: could not find project Gopkg.toml, use dep init to initiate a manifest",
+            str(ex.exception))
+
+    def test_builds_project_with_remote_deps(self):
+        source_dir = os.path.join(self.TEST_DATA_FOLDER, "remote-deps")
+
+        self.builder.build(source_dir, self.artifacts_dir, self.scratch_dir,
+                           os.path.join(source_dir, "Gopkg.toml"),
+                           runtime=self.runtime,
+                           options={"handler": "main"})
+
+        expected_files = {"main"}
+        output_files = set(os.listdir(self.artifacts_dir))
+
+        self.assertEquals(expected_files, output_files)
+
+    def test_builds_project_with_failed_remote_deps(self):
+        source_dir = os.path.join(self.TEST_DATA_FOLDER, "failed-remote")
+
+        with self.assertRaises(WorkflowFailedError) as ex:
+            self.builder.build(source_dir, self.artifacts_dir, self.scratch_dir,
+                            os.path.join(source_dir, "Gopkg.toml"),
+                            runtime=self.runtime,
+                            options={"handler": "main"})
+
+        # The full message is super long, so part of it is fine.
+        self.assertNotEqual(str(ex.exception).find('unable to deduce repository and source type for'), -1)
