695e00f2b75b573e3d4704b91c5750aca00118ec
Simon Thulbourn <simon+github@thulbourn.com>
2018-12-30 17:16:52 +0100

    cleans up actions

--- aws_lambda_builders/workflows/go_dep/actions.py
+++ aws_lambda_builders/workflows/go_dep/actions.py
@@ -5,7 +5,7 @@ Actions for Go dependency resolution with dep
 import logging
 import os
 
-from shutil import copyfile
+from shutil import copyfile, SameFileError
 
 from aws_lambda_builders.actions import BaseAction, Purpose, ActionFailedError
 
@@ -13,30 +13,6 @@ from .exec import ExecutionError
 
 
 LOG = logging.getLogger(__name__)
-
-class CopyFileAction(BaseAction):
-
-    """
-    Copies a file to the source dir
-    """
-
-    NAME = "CopyFile"
-    DESCRIPTION = "Copies a file to source directory"
-    PURPOSE = Purpose.COPY_SOURCE
-
-    def __init__(self, manifest_path, source_dir):
-        super(CopyFileAction, self).__init__()
-
-        self.manifest_path = manifest_path
-        self.source_dir = source_dir
-
-    def execute(self):
-        manifest_name = os.path.basename(self.manifest_path)
-
-        try:
-            copyfile(self.manifest_path, os.path.join(self.source_dir, manifest_name))
-        except OSError as ex:
-            raise ActionFailedError(str(ex))
 
 class DepEnsureAction(BaseAction):
 
@@ -48,22 +24,16 @@ class DepEnsureAction(BaseAction):
     DESCRIPTION = "Ensures all dependencies are installed for a project"
     PURPOSE = Purpose.RESOLVE_DEPENDENCIES
 
-    def __init__(self, go_dir, source_dir, subprocess_dep):
+    def __init__(self, base_dir, subprocess_dep):
         super(DepEnsureAction, self).__init__()
 
-        self.go_dir = go_dir
-        self.source_dir = source_dir
+        self.base_dir = base_dir
         self.subprocess_dep = subprocess_dep
 
     def execute(self):
-        env = {"GOPATH": self.go_dir, "PATH": os.environ["PATH"]}
-
-        LOG.debug("Environment variables: {}".format(env))
-
         try:
             self.subprocess_dep.run(["ensure", "-v"],
-                                    cwd=self.source_dir,
-                                    env=env)
+                                    cwd=self.base_dir)
         except ExecutionError as ex:
             raise ActionFailedError(str(ex))
 
@@ -77,22 +47,22 @@ class GoBuildAction(BaseAction):
     DESCRIPTION = "Builds final binary"
     PURPOSE = Purpose.COMPILE_SOURCE
 
-    def __init__(self, go_dir, source_dir, handler, subprocess_go):
+    def __init__(self, base_dir, source_path, output_path, subprocess_go):
         super(GoBuildAction, self).__init__()
 
-        self.go_dir = go_dir
-        self.source_dir = source_dir
-        self.handler = handler
+        self.base_dir = base_dir
+        self.source_path = source_path
+        self.output_path = output_path
+
         self.subprocess_go = subprocess_go
 
     def execute(self):
-        env = {"GOPATH": self.go_dir, "PATH": os.environ["PATH"], "GOOS": "linux", "GOARCH": "amd64"}
-
-        LOG.debug("Environment variables: {}".format(env))
+        env = {}
+        env.update(os.environ)
+        env.update({"GOOS": "linux", "GOARCH": "amd64"})
 
         try:
-            self.subprocess_go.run(["build", "-o", self.handler],
-                                    cwd=self.source_dir,
-                                    env=env)
+            self.subprocess_go.run(["build", "-o", self.output_path, self.source_path],
+                                    cwd=self.source_path, env=env)
         except ExecutionError as ex:
             raise ActionFailedError(str(ex))
