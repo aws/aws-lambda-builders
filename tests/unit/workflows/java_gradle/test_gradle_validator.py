82c9db6732d86de95ecf1d4dd5a2811f6adc6d11
Dongie Agnir <dongie@amazon.com>
2019-02-12 11:57:21 -0800

    Emit warning when version string not found
    When the executable has an error return code when we run 'gradle -v', we also
    treat that as a "version not found".

--- tests/unit/workflows/java_gradle/test_gradle_validator.py
+++ tests/unit/workflows/java_gradle/test_gradle_validator.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from mock import patch, Mock
 from parameterized import parameterized
-from aws_lambda_builders.workflows.java_gradle.gradle_validator import GradleBinaryValidator, GradleBinaryValidatorError
+from aws_lambda_builders.workflows.java_gradle.gradle_validator import GradleBinaryValidator
 
 
 class FakePopen(object):
@@ -24,6 +24,8 @@ class TestGradleBinaryValidator(TestCase):
     @patch("aws_lambda_builders.workflows.java_gradle.utils.OSUtils")
     def setUp(self, MockOSUtils):
         self.mock_os_utils = MockOSUtils.return_value
+        self.mock_log = Mock()
+        self.gradle_path = '/path/to/gradle'
 
     @parameterized.expand([
         '1.7.0',
@@ -32,21 +34,18 @@ class TestGradleBinaryValidator(TestCase):
     ])
     def test_accepts_any_jvm_mv(self, version):
         version_string = ('JVM:          %s' % version).encode()
-        gradle_path = '/path/to/gradle'
         self.mock_os_utils.popen.side_effect = [FakePopen(stdout=version_string)]
         validator = GradleBinaryValidator(os_utils=self.mock_os_utils)
-        self.assertTrue(validator.validate(gradle_path=gradle_path))
-        self.assertEqual(validator.validated_binary_path, gradle_path)
+        self.assertTrue(validator.validate(gradle_path=self.gradle_path))
+        self.assertEqual(validator.validated_binary_path, self.gradle_path)
 
     def test_emits_warning_when_jvm_mv_greater_than_8(self):
         version_string = 'JVM:          9.0.0'.encode()
-        gradle_path = '/path/to/gradle'
-        mock_log = Mock()
         self.mock_os_utils.popen.side_effect = [FakePopen(stdout=version_string)]
-        validator = GradleBinaryValidator(os_utils=self.mock_os_utils, log=mock_log)
-        self.assertTrue(validator.validate(gradle_path=gradle_path))
-        self.assertEqual(validator.validated_binary_path, gradle_path)
-        mock_log.warning.assert_called_with(GradleBinaryValidator.MAJOR_VERSION_WARNING, gradle_path, '9')
+        validator = GradleBinaryValidator(os_utils=self.mock_os_utils, log=self.mock_log)
+        self.assertTrue(validator.validate(gradle_path=self.gradle_path))
+        self.assertEqual(validator.validated_binary_path, self.gradle_path)
+        self.mock_log.warning.assert_called_with(GradleBinaryValidator.MAJOR_VERSION_WARNING, self.gradle_path, '9')
 
     @parameterized.expand([
         '1.6.0',
@@ -55,18 +54,22 @@ class TestGradleBinaryValidator(TestCase):
     ])
     def test_does_not_emit_warning_when_jvm_mv_8_or_less(self, version):
         version_string = ('JVM:          %s' % version).encode()
-        gradle_path = '/path/to/gradle'
-        mock_log = Mock()
         self.mock_os_utils.popen.side_effect = [FakePopen(stdout=version_string)]
-        validator = GradleBinaryValidator(os_utils=self.mock_os_utils, log=mock_log)
-        self.assertTrue(validator.validate(gradle_path=gradle_path))
-        self.assertEqual(validator.validated_binary_path, gradle_path)
-        mock_log.warning.assert_not_called()
+        validator = GradleBinaryValidator(os_utils=self.mock_os_utils, log=self.mock_log)
+        self.assertTrue(validator.validate(gradle_path=self.gradle_path))
+        self.assertEqual(validator.validated_binary_path, self.gradle_path)
+        self.mock_log.warning.assert_not_called()
 
-    def test_gradle_executable_fails(self):
-        version_string = 'java version "1.8.0"'.encode()
+    def test_emits_warning_when_gradle_excutable_fails(self):
+        version_string = 'JVM:          9.0.0'.encode()
         self.mock_os_utils.popen.side_effect = [FakePopen(stdout=version_string, returncode=1)]
-        validator = GradleBinaryValidator(os_utils=self.mock_os_utils)
-        with self.assertRaises(GradleBinaryValidatorError) as raised:
-            validator.validate(gradle_path='/path/to/gradle')
-        self.assertEqual(raised.exception.args[0], GradleBinaryValidatorError.MESSAGE)
+        validator = GradleBinaryValidator(os_utils=self.mock_os_utils, log=self.mock_log)
+        validator.validate(gradle_path=self.gradle_path)
+        self.mock_log.warning.assert_called_with(GradleBinaryValidator.VERSION_STRING_WARNING, self.gradle_path)
+
+    def test_emits_warning_when_version_string_not_found(self):
+        version_string = 'The Java Version:          9.0.0'.encode()
+        self.mock_os_utils.popen.side_effect = [FakePopen(stdout=version_string, returncode=0)]
+        validator = GradleBinaryValidator(os_utils=self.mock_os_utils, log=self.mock_log)
+        validator.validate(gradle_path=self.gradle_path)
+        self.mock_log.warning.assert_called_with(GradleBinaryValidator.VERSION_STRING_WARNING, self.gradle_path)
