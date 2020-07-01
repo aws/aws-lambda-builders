bb938dbfeb77dbfa51f483ea7e39ccea20357030
softprops <d.tangren@gmail.com>
2020-06-30 20:22:25 -0400

    update integration tests for rust cargo with latest aws rust runtime interfaces

--- tests/integration/workflows/rust_cargo/testdata/workspaces/foo/src/main.rs
+++ tests/integration/workflows/rust_cargo/testdata/workspaces/foo/src/main.rs
@@ -1,10 +1,10 @@
-use lambda::lambda;
+use lambda::{lambda, Context};
 use serde_json::Value;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[lambda]
 #[tokio::main]
-async fn main(_: Value) -> Result<Value, Error> {
+async fn main(_: Value, _: Context) -> Result<Value, Error> {
     Ok("foo".into())
 }
