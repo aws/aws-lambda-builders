880a9fd2dbbd572745f921472e71418a4d3011de
Sriram Madapusi Vasudevan <3770774+sriram-mv@users.noreply.github.com>
2021-08-25 13:42:15 -0700

    fix(go version parts): remove alphabets from the version for validation (#259)
    * fix(go version parts): remove alphabets from the version for validation
    
    - Go versions like 1.12rc1 or 1.16beta1 are supported.
    - Test added.
    
    * fix: use regex for go versions

--- aws_lambda_builders/workflows/go_modules/validator.py
+++ aws_lambda_builders/workflows/go_modules/validator.py
@@ -3,6 +3,7 @@ Go Runtime Validation
 """
 
 import logging
+import re
 import os
 import subprocess
 
@@ -12,9 +13,9 @@ LOG = logging.getLogger(__name__)
 
 
 class GoRuntimeValidator(object):
-
     LANGUAGE = "go"
     SUPPORTED_RUNTIMES = {"go1.x"}
+    GO_VERSION_REGEX = re.compile("go(\\d)\\.(x|\\d+)")
 
     def __init__(self, runtime):
         self.runtime = runtime
@@ -27,6 +28,15 @@ class GoRuntimeValidator(object):
         :return bool: True, if the runtime is supported.
         """
         return self.runtime in self.SUPPORTED_RUNTIMES
+
+    @staticmethod
+    def get_go_versions(version_string):
+        parts = GoRuntimeValidator.GO_VERSION_REGEX.findall(version_string)
+        try:
+            # NOTE(sriram-mv): The version parts need to be a list with a major and minor version.
+            return int(parts[0][0]), int(parts[0][1])
+        except IndexError:
+            return 0, 0
 
     def validate(self, runtime_path):
         """
@@ -42,16 +52,13 @@ class GoRuntimeValidator(object):
         min_expected_minor_version = 11 if expected_major_version == 1 else 0
 
         p = subprocess.Popen([runtime_path, "version"], cwd=os.getcwd(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, _ = p.communicate()
+        version_string, _ = p.communicate()
 
         if p.returncode == 0:
-            out_parts = out.decode().split()
-            if len(out_parts) >= 3:
-                version_parts = [int(x.replace("rc", "")) for x in out_parts[2].replace(self.LANGUAGE, "").split(".")]
-                if len(version_parts) >= 2:
-                    if version_parts[0] == expected_major_version and version_parts[1] >= min_expected_minor_version:
-                        self._valid_runtime_path = runtime_path
-                        return self._valid_runtime_path
+            major_version, minor_version = GoRuntimeValidator.get_go_versions(version_string.decode())
+            if major_version == expected_major_version and minor_version >= min_expected_minor_version:
+                self._valid_runtime_path = runtime_path
+                return self._valid_runtime_path
 
         # otherwise, raise mismatch exception
         raise MisMatchRuntimeError(language=self.LANGUAGE, required_runtime=self.runtime, runtime_path=runtime_path)
