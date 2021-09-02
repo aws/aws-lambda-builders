b8414590eb28ef21fe21bb241376902be2bce5ce
mingkun2020 <68391979+mingkun2020@users.noreply.github.com>
2021-09-02 16:19:56 -0700

    feat/Node and Ruby Dependency Dir and Download Dependency (#272)
    * add download dependencies and dependencies folder for Node and Ruby
    
    * black reformat
    
    * added unit tests and integration tests
    
    * add logging message when download_dependencies is False and dependencies_dir is None, also update testing
    
    * word smithing

--- tests/unit/workflows/ruby_bundler/test_workflow.py
+++ tests/unit/workflows/ruby_bundler/test_workflow.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from aws_lambda_builders.actions import CopySourceAction
+from aws_lambda_builders.actions import CopySourceAction, CopyDependenciesAction
 from aws_lambda_builders.workflows.ruby_bundler.workflow import RubyBundlerWorkflow
 from aws_lambda_builders.workflows.ruby_bundler.actions import RubyBundlerInstallAction, RubyBundlerVendorAction
 
@@ -11,9 +11,42 @@ class TestRubyBundlerWorkflow(TestCase):
     this is just a quick wiring test to provide fast feedback if things are badly broken
     """
 
-    def test_workflow_sets_up_bundler_actions(self):
-        workflow = RubyBundlerWorkflow("source", "artifacts", "scratch_dir", "manifest")
+    def test_workflow_sets_up_bundler_actions_with_download_dependencies_without_dependencies_dir(self):
+        workflow = RubyBundlerWorkflow(
+            "source", "artifacts", "scratch_dir", "manifest", dependencies_dir="dep", download_dependencies=False
+        )
         self.assertEqual(len(workflow.actions), 3)
         self.assertIsInstance(workflow.actions[0], CopySourceAction)
         self.assertIsInstance(workflow.actions[1], RubyBundlerInstallAction)
         self.assertIsInstance(workflow.actions[2], RubyBundlerVendorAction)
+
+    def test_workflow_sets_up_npm_actions_without_download_dependencies_with_dependencies_dir(self):
+        workflow = RubyBundlerWorkflow(
+            "source", "artifacts", "scratch_dir", "manifest", dependencies_dir="dep", download_dependencies=False
+        )
+
+        self.assertEqual(len(workflow.actions), 2)
+
+        self.assertIsInstance(workflow.actions[0], CopySourceAction)
+        self.assertIsInstance(workflow.actions[1], CopySourceAction)
+
+    def test_workflow_sets_up_bundler_actions_with_download_dependencies_without_dependencies_dir(self):
+        workflow = RubyBundlerWorkflow(
+            "source", "artifacts", "scratch_dir", "manifest", dependencies_dir="dep", download_dependencies=True
+        )
+
+        self.assertEqual(len(workflow.actions), 4)
+
+        self.assertIsInstance(workflow.actions[0], CopySourceAction)
+        self.assertIsInstance(workflow.actions[1], RubyBundlerInstallAction)
+        self.assertIsInstance(workflow.actions[2], RubyBundlerVendorAction)
+        self.assertIsInstance(workflow.actions[3], CopyDependenciesAction)
+
+    def test_workflow_sets_up_bundler_actions_without_download_dependencies_without_dependencies_dir(self):
+        workflow = RubyBundlerWorkflow(
+            "source", "artifacts", "scratch_dir", "manifest", dependencies_dir=None, download_dependencies=False
+        )
+
+        self.assertEqual(len(workflow.actions), 1)
+
+        self.assertIsInstance(workflow.actions[0], CopySourceAction)
