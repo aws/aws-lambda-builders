1a80be4ce9d7469829bf988513e8788253aed47b
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2022-02-16 21:26:48 -0800

    add integration tests for changes

---
+++ tests/integration/test_actions.py
@@ -0,0 +1,59 @@
+import os
+from pathlib import Path
+import tempfile
+from unittest import TestCase
+from parameterized import parameterized
+
+
+from aws_lambda_builders.actions import CopyDependenciesAction, MoveDependenciesAction
+from aws_lambda_builders.utils import copytree
+
+
+class TestCopyDependenciesAction(TestCase):
+
+    @parameterized.expand([
+        ("single_file",),
+        ("multiple_files",),
+        ("empty_subfolders",),
+    ])
+    def test_copy_dependencies_action(self, source_folder):
+        curr_dir = Path(__file__).resolve().parent
+        test_folder = os.path.join(curr_dir, "testdata", source_folder)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            empty_source = os.path.join(tmpdir, "empty_source")
+            target = os.path.join(tmpdir, "target")
+
+            os.mkdir(empty_source)
+            os.mkdir(target)
+
+            copy_dependencies_action = CopyDependenciesAction(empty_source, test_folder, target)
+            copy_dependencies_action.execute()
+
+            self.assertEqual(os.listdir(test_folder), os.listdir(target))
+
+
+class TestMoveDependenciesAction(TestCase):
+
+    @parameterized.expand([
+        ("single_file",),
+        ("multiple_files",),
+        ("empty_subfolders",),
+    ])
+    def test_move_dependencies_action(self, source_folder):
+        curr_dir = Path(__file__).resolve().parent
+        test_folder = os.path.join(curr_dir, "testdata", source_folder)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            test_source = os.path.join(tmpdir, "test_source")
+            empty_source = os.path.join(tmpdir, "empty_source")
+            target = os.path.join(tmpdir, "target")
+
+            os.mkdir(test_source)
+            os.mkdir(empty_source)
+            os.mkdir(target)
+
+            copytree(test_folder, test_source)
+
+            move_dependencies_action = MoveDependenciesAction(empty_source, test_source, target)
+            move_dependencies_action.execute()
+
+            self.assertEqual(os.listdir(test_folder), os.listdir(target))
