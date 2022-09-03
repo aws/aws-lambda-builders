ab39fa5be8bff6cb943ddde4524b5f18a80fdd4a
David Calavera <david.calavera@gmail.com>
2022-09-02 21:47:09 -0700

    Remove type hints
    They're causing false positives in Python 3.9 with pylint.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- aws_lambda_builders/workflows/rust_cargo/feature_flag.py
+++ aws_lambda_builders/workflows/rust_cargo/feature_flag.py
@@ -1,12 +1,11 @@
 """
 Cargo Lambda specific feature flag utilities
 """
-from typing import Optional, List
 
 EXPERIMENTAL_FLAG_CARGO_LAMBDA = "experimentalCargoLambda"
 
 
-def is_experimental_cargo_lambda_scope(experimental_flags: Optional[List[str]]) -> bool:
+def is_experimental_cargo_lambda_scope(experimental_flags):
     """
     A function which will determine if experimental Cargo Lambda scope is active
     """
