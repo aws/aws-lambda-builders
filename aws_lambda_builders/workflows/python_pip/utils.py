838d6bf196acf2a1d8efd2cd7e08fe33d8afe71c
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2022-11-22 01:10:08 -0800

    Remove redundant code/tests

--- aws_lambda_builders/workflows/python_pip/utils.py
+++ aws_lambda_builders/workflows/python_pip/utils.py
@@ -56,29 +56,6 @@ class OSUtils(object):
         with zipfile.ZipFile(zipfile_path, "r") as z:
             z.extractall(unpack_dir)
 
-    def extract_tarfile(self, tarfile_path, unpack_dir):
-        with tarfile.open(tarfile_path, "r:*") as tar:
-
-            def is_within_directory(directory, target):
-
-                abs_directory = os.path.abspath(directory)
-                abs_target = os.path.abspath(target)
-
-                prefix = os.path.commonprefix([abs_directory, abs_target])
-
-                return prefix == abs_directory
-
-            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-
-                for member in tar.getmembers():
-                    member_path = os.path.join(path, member.name)
-                    if not is_within_directory(path, member_path):
-                        raise Exception("Attempted Path Traversal in Tar File")
-
-                tar.extractall(path, members, numeric_owner=numeric_owner)
-
-            safe_extract(tar, unpack_dir)
-
     def directory_exists(self, path):
         return os.path.isdir(path)
 
