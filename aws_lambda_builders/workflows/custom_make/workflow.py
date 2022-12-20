ef4ab5872d083b03be3652fd98fede5023f62ec5
Ruperto Torres <86501267+torresxb1@users.noreply.github.com>
2022-12-20 11:02:00 -0800

    feat: implement build in source for custom make (#417)

--- aws_lambda_builders/workflows/custom_make/workflow.py
+++ aws_lambda_builders/workflows/custom_make/workflow.py
@@ -31,11 +31,10 @@ class CustomMakeWorkflow(BaseWorkflow):
 
         self.os_utils = OSUtils()
 
-        # Find the logical id of the function to be built.
         options = kwargs.get("options") or {}
-        build_logical_id = options.get("build_logical_id", None)
-        working_directory = options.get("working_directory", scratch_dir)
+        build_in_source = kwargs.get("build_in_source")
 
+        build_logical_id = options.get("build_logical_id", None)
         if not build_logical_id:
             raise WorkflowFailedError(
                 workflow_name=self.NAME,
@@ -45,9 +44,17 @@ class CustomMakeWorkflow(BaseWorkflow):
 
         subprocess_make = SubProcessMake(make_exe=self.binaries["make"].binary_path, osutils=self.os_utils)
 
+        # Don't build in source by default (backwards compatibility)
+        if build_in_source is None:
+            build_in_source = False
+
+        # an explicitly definied working directory should take precedence
+        working_directory = options.get("working_directory") or self._select_working_directory(
+            source_dir, scratch_dir, build_in_source
+        )
+
         make_action = CustomMakeAction(
             artifacts_dir,
-            scratch_dir,
             manifest_path,
             osutils=self.os_utils,
             subprocess_make=subprocess_make,
@@ -55,7 +62,19 @@ class CustomMakeWorkflow(BaseWorkflow):
             working_directory=working_directory,
         )
 
-        self.actions = [CopySourceAction(source_dir, scratch_dir, excludes=self.EXCLUDED_FILES), make_action]
+        self.actions = []
+
+        if not self.build_in_source:
+            # if we're building on scratch_dir, we have to first copy the source there
+            self.actions.append(CopySourceAction(source_dir, scratch_dir, excludes=self.EXCLUDED_FILES))
+
+        self.actions.append(make_action)
+
+    def _select_working_directory(self, source_dir: str, scratch_dir: str, build_in_source: bool):
+        """
+        Returns the directory where the make action should be executed
+        """
+        return source_dir if build_in_source else scratch_dir
 
     def get_resolvers(self):
         return [PathResolver(runtime="provided", binary="make", executable_search_paths=self.executable_search_paths)]
