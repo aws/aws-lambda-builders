aa47dc2ea27101129c59f9b5095ea7475b2780a5
Daniel Mil <84205762+mildaniel@users.noreply.github.com>
2023-06-05 12:02:28 -0700

    Version bump to 1.33.0 (#511)

--- aws_lambda_builders/__init__.py
+++ aws_lambda_builders/__init__.py
@@ -4,5 +4,5 @@ AWS Lambda Builder Library
 
 # Changing version will trigger a new release!
 # Please make the version change as the last step of your development.
-__version__ = "1.32.0"
+__version__ = "1.33.0"
 RPC_PROTOCOL_VERSION = "0.3"
