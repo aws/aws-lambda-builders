8bac5efcea9a44c810fdca9c4c8b29967cf47dc0
Sean O Brien <briensea@amazon.com>
2023-09-05 11:25:36 +0000

    Add python3.12.

--- tests/integration/workflows/python_pip/test_python_pip.py
+++ tests/integration/workflows/python_pip/test_python_pip.py
@@ -18,7 +18,7 @@ from aws_lambda_builders.workflows.python_pip.utils import EXPERIMENTAL_FLAG_BUI
 logger = logging.getLogger("aws_lambda_builders.workflows.python_pip.workflow")
 IS_WINDOWS = platform.system().lower() == "windows"
 NOT_ARM = platform.processor() != "aarch64"
-ARM_RUNTIMES = {"python3.8", "python3.9", "python3.10", "python3.11"}
+ARM_RUNTIMES = {"python3.8", "python3.9", "python3.10", "python3.11", "python3.12"}
 
 
 @parameterized_class(("experimental_flags",), [([]), ([EXPERIMENTAL_FLAG_BUILD_PERFORMANCE])])
@@ -64,6 +64,7 @@ class TestPythonPipWorkflow(TestCase):
             "python3.9": "python3.7",
             "python3.10": "python3.9",
             "python3.11": "python3.10",
+            "python3.12": "python3.11",
         }
 
     def tearDown(self):
@@ -96,7 +97,7 @@ class TestPythonPipWorkflow(TestCase):
             experimental_flags=self.experimental_flags,
         )
 
-        if self.runtime in ("python3.10", "python3.11"):
+        if self.runtime in ("python3.10", "python3.11", "python3.12"):
             self.check_architecture_in("numpy-1.23.5.dist-info", ["manylinux2014_x86_64", "manylinux1_x86_64"])
             expected_files = self.test_data_files.union({"numpy", "numpy-1.23.5.dist-info", "numpy.libs"})
         else:
@@ -123,7 +124,7 @@ class TestPythonPipWorkflow(TestCase):
                 executable_search_paths=[executable_dir],
             )
 
-            if self.runtime in ("python3.10", "python3.11"):
+            if self.runtime in ("python3.10", "python3.11", "python3.12"):
                 self.check_architecture_in("numpy-1.23.5.dist-info", ["manylinux2014_x86_64", "manylinux1_x86_64"])
                 expected_files = self.test_data_files.union({"numpy", "numpy-1.23.5.dist-info", "numpy.libs"})
             else:
@@ -168,12 +169,12 @@ class TestPythonPipWorkflow(TestCase):
             experimental_flags=self.experimental_flags,
         )
         expected_files = self.test_data_files.union({"numpy", "numpy.libs", "numpy-1.20.3.dist-info"})
-        if self.runtime in ("python3.10", "python3.11"):
+        if self.runtime in ("python3.10", "python3.11", "python3.12"):
             expected_files = self.test_data_files.union({"numpy", "numpy.libs", "numpy-1.23.5.dist-info"})
         output_files = set(os.listdir(self.artifacts_dir))
         self.assertEqual(expected_files, output_files)
 
-        if self.runtime in ("python3.10", "python3.11"):
+        if self.runtime in ("python3.10", "python3.11", "python3.12"):
             self.check_architecture_in("numpy-1.23.5.dist-info", ["manylinux2014_aarch64"])
         else:
             self.check_architecture_in("numpy-1.20.3.dist-info", ["manylinux2014_aarch64"])
