75f3797d562977a2f68b5dbdf89d8568d786dcf8
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2024-02-06 17:10:32 -0800

    add missing key for runtime mismatch tests

--- tests/integration/workflows/python_pip/test_python_pip.py
+++ tests/integration/workflows/python_pip/test_python_pip.py
@@ -60,6 +60,7 @@ class TestPythonPipWorkflow(TestCase):
         )
         self.runtime_mismatch = {
             "python3.8": "python3.9",
+            "python3.9": "python3.10",
             "python3.10": "python3.9",
             "python3.11": "python3.10",
             "python3.12": "python3.11",
