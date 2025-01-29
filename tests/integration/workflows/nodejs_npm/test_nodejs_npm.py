baeafc6d505f64fadba5ae057e970b98705c2a9b
saraiyakush <52939390+saraiyakush@users.noreply.github.com>
2025-01-29 10:53:54 +0530

    Fix tests for Node after SAM_NPM_RUN_TEST_WITH_BUILD

--- tests/integration/workflows/nodejs_npm/test_nodejs_npm.py
+++ tests/integration/workflows/nodejs_npm/test_nodejs_npm.py
@@ -781,6 +781,7 @@ class TestNodejsNpmWorkflow(TestCase):
         self.assertTrue(all(expected_module in source_modules for expected_module in expected_modules))
 
     @parameterized.expand([("nodejs16.x",), ("nodejs18.x",), ("nodejs20.x",)])
+    @mock.patch.dict("os.environ", {"SAM_NPM_RUN_TEST_WITH_BUILD": "true"}, clear=True)
     def test_runs_test_script_if_specified(self, runtime):
         source_dir = os.path.join(self.TEST_DATA_FOLDER, "test-script-to-create-file")
 
@@ -793,6 +794,22 @@ class TestNodejsNpmWorkflow(TestCase):
         )
 
         expected_files = {"package.json", "created.js"}
+        output_files = set(os.listdir(self.artifacts_dir))
+        self.assertEqual(expected_files, output_files)
+
+    @parameterized.expand([("nodejs16.x",), ("nodejs18.x",), ("nodejs20.x",)])
+    def test_does_not_run_test_script_if_env_var_not_specified(self, runtime):
+        source_dir = os.path.join(self.TEST_DATA_FOLDER, "test-script-to-create-file")
+
+        self.builder.build(
+            source_dir,
+            self.artifacts_dir,
+            self.scratch_dir,
+            os.path.join(source_dir, "package.json"),
+            runtime=runtime,
+        )
+
+        expected_files = {"package.json"}
         output_files = set(os.listdir(self.artifacts_dir))
         self.assertEqual(expected_files, output_files)
 
